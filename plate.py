2ab1b02a403d870cada0bce34d192f398d06c2e1
daniel.kurushin@gmail.com <daniel.kurushin@gmail.com>
2020-02-07 12:32:17 +0500

    imports

--- plate.py
+++ plate.py
@@ -1,7 +1,7 @@
 from glob import glob
 from PIL import Image, ImageOps
-from digits.digits import coordinates
-SIZE = 16
+from digits.digits import digit_coordinates, plate_coordinates, digit_values
+SIZE = 32
 e = 2.78
 
 def σ(x):
@@ -21,11 +21,15 @@ digits = ImageOps.grayscale(Image.open(open('digits/digits.png', 'rb')))
 for x in glob('img/*.png'):
     print(x)
     img = ImageOps.grayscale(Image.open(open(x, 'rb')))
-    for x1, y1, x2, y2 in coordinates:
+    j = 0
+    for x1, y1, x2, y2 in plate_coordinates:
         b_digit = img.crop((x1, y1, x2, y2)).resize(size = (SIZE,SIZE), resample = Image.HAMMING)
         rez = []
-        for i in range(10):
-            a_digit = digits.crop((62*i, 0, 62*(i+1), 82)).resize(size = (SIZE,SIZE), resample = Image.HAMMING)
+        i = 0
+        for a, b, c, d in digit_coordinates:
+            a_digit = digits.crop((a, b, c, d)).resize(size = (SIZE,SIZE), resample = Image.HAMMING)
             d = compare(a_digit, b_digit)
-            rez += [(i, d)]
+            rez += [(digit_values[i], d)]
+            i += 1
+        j += 1
         print(sorted(rez, key = lambda x : x[1])[0][0])
