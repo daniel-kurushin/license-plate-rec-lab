2990bfaf326dfcae5d5c2dae37ece655983f6704
daniel.kurushin@gmail.com <daniel.kurushin@gmail.com>
2020-02-11 15:25:21 +0500

    analize.py

--- analize.py
+++ analize.py
@@ -1,15 +1,20 @@
-from glob import glob
+from json import load
+import matplotlib.pyplot as plt
 
+D = load(open('D.json'))
+w = load(open('w.json'))
 
-for log in glob('??'):
-    print(int(log), end = ' ')
-    n, m = 0, 0
-    for line in [ _.strip('\n') for _ in open(log).readlines() ]:
-        _in = line.split(':')[0].strip()
-        _out = line.split(';')[1].strip()
-        i = 0
-        for c in _out:
-            if c == _in[i]: m += 1
-            i += 1
-            n += 1
-    print(m / n)
+SIZE = 16
+
+d0 = w
+img = []
+i = 0
+for x in range(SIZE):
+    col = []
+    for y in range(SIZE):
+        col += [d0[i]]
+        i += 1
+    img += [col]
+    
+plt.imshow(img)
+plt.show()
